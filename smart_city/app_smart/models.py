2b79621853e10232b5f0ae4d8c26b443120c0462
Jo���o Victor Borges de <49434523885@>
2024-06-13 16:43:30 -0300

    novos dados

---
+++ smart_city/app_smart/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+
+# Create your models here.
+
+#tabela sensor
+class Sensor(models.Model):
+    TIPOS_SENSOR_CHOICES=[
+        ('Temperatura', 'Temperatura'),
+        ('Umidade', 'Umidade'),
+        ('Contador','Conatdor'),
+        ('Luminosidade', 'Luminosidade')
+    ]
+    tipo = models.CharField(max_length=50, choices=TIPOS_SENSOR_CHOICES)
+    mac_address = models.CharField(max_length=20, null=True)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    localizacao = models.CharField(max_length=100)
+    responsavel = models.CharField(max_length=100)
+    unidade_medida = models.CharField(max_length=20, blank=True, null=True)
+    status_operacional = models.BooleanField(default=True)
+    observacao = models.TextField(blank=True)
+    
+    def __str__(self):
+        return f"{self.tipo} -{self.localizacao}"
+
+class TemperaturaData(models.Model):
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
+    valor = models.FloatField()
+    #timestamp = models.DataTimeField(auto_now_add = True)
+    timestamp = models.DateField()
+    
+    def __str__(self):
+        return f"Temperatura {self.valor} C - {self.timestamp}"
+
+class Umidade(models.Model):
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
+    valor = models.FloatField()
+    timestamp = models.DateTimeField()
+    
+    def __str__(self):
+        return f"Umidade {self.valor} % -{self.timestamp}"
+    
+class ContadorData(models.Model):
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField()
+    
+    def __str__(self):
+        return f"contagem {self.sensor} - {self.timestamp}"
+    
+class LuminosidadeData(models.Model):
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
+    valor = models.FloatField()
+    timestamp = models.DateTimeField()
+    
+    
+    def __str__(self):
+        return f"Luminosidade {self.valor} - {self.timestamp}"
--- smarty_city/app_smart/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def abre_index(request):
-    return HttpResponse("Bem vindo ao smart")
